32bb1939ad80ae33a10e71f34ce76ffa515fe335
Filip Synek <filip@synek.dev>
2024-06-10 12:37:43 +0200

    End-to-end test `GetAllTeams` endpoint on `TeamsController`

---
+++ src/test/kotlin/dev/synek/puckmetrics/features/teams/TeamsControllerEndToEndTest.kt
@@ -0,0 +1,85 @@
+package dev.synek.puckmetrics.features.teams
+
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.springframework.test.context.jdbc.Sql
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+import org.springframework.transaction.annotation.Transactional
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.utility.DockerImageName
+
+@Testcontainers
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Transactional
+@AutoConfigureMockMvc
+@Sql("/test-data/teams.sql")
+class TeamsControllerEndToEndTest(
+    @Autowired private val mockMvc: MockMvc
+) {
+    companion object {
+        @Container
+        val postgresContainer = PostgreSQLContainer<Nothing>(
+            DockerImageName.parse("postgres:16")
+        ).apply {
+            withDatabaseName("integration-tests-db")
+            withUsername("test-user")
+            withPassword("test-password")
+        }
+
+        @JvmStatic
+        @DynamicPropertySource
+        fun datasourceConfig(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", postgresContainer::getJdbcUrl)
+            registry.add("spring.datasource.password", postgresContainer::getPassword)
+            registry.add("spring.datasource.username", postgresContainer::getUsername)
+        }
+    }
+
+    private val baseUrl = "/teams"
+
+    @Nested
+    inner class GetAllTeams {
+        @Test
+        @Sql("/test-data/empty.sql")
+        fun `empty list when no teams are present`() {
+            mockMvc.get(baseUrl)
+                .andExpect {
+                    status { isOk() }
+                    content { contentType("application/json") }
+                    content { json("[]") }
+                }
+        }
+
+        @Test
+        fun `list of all teams`() {
+            mockMvc.get(baseUrl)
+                .andExpect {
+                    status { isOk() }
+                    content { contentType("application/json") }
+                    jsonPath("$[0].location") { value("Boston") }
+                    jsonPath("$[0].name") { value("Bruins") }
+                    jsonPath("$[1].location") { value("Chicago") }
+                    jsonPath("$[1].name") { value("Blackhawks") }
+                    jsonPath("$[2].location") { value("Detroit") }
+                    jsonPath("$[2].name") { value("Red Wings") }
+                    jsonPath("$[3].location") { value("Montreal") }
+                    jsonPath("$[3].name") { value("Canadiens") }
+                    jsonPath("$[4].location") { value("New York") }
+                    jsonPath("$[4].name") { value("Rangers") }
+                    jsonPath("$[5].location") { value("Toronto") }
+                    jsonPath("$[5].name") { value("Maple Leafs") }
+                }
+        }
+
+    }
+}
---
+++ src/test/resources/test-data/teams.sql
@@ -0,0 +1,17 @@
+INSERT INTO public.team (id, franchise_id, location, name, abbreviation)
+VALUES (1, 1, 'Boston', 'Bruins', 'BOS');
+
+INSERT INTO public.team (id, franchise_id, location, name, abbreviation)
+VALUES (2, 2, 'Chicago', 'Blackhawks', 'CHI');
+
+INSERT INTO public.team (id, franchise_id, location, name, abbreviation)
+VALUES (3, 3, 'Detroit', 'Red Wings', 'DET');
+
+INSERT INTO public.team (id, franchise_id, location, name, abbreviation)
+VALUES (4, 4, 'Montreal', 'Canadiens', 'MTL');
+
+INSERT INTO public.team (id, franchise_id, location, name, abbreviation)
+VALUES (5, 5, 'New York', 'Rangers', 'NYR');
+
+INSERT INTO public.team (id, franchise_id, location, name, abbreviation)
+VALUES (6, 6, 'Toronto', 'Maple Leafs', 'TOR');
