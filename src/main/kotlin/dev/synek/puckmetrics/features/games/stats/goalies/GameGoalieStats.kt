1a33c0d46b5f705182afd64002295da7b19b1f93
Filip Synek <filip@synek.dev>
2024-06-15 13:36:20 +0200

    Add `GameGoalieStats` entity

---
+++ src/main/kotlin/dev/synek/puckmetrics/features/games/stats/goalies/GameGoalieStats.kt
@@ -0,0 +1,85 @@
+package dev.synek.puckmetrics.features.games.stats.goalies
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.IdClass
+import java.io.Serializable
+
+@Entity
+@IdClass(GameGoalieStatsId::class)
+data class GameGoalieStats(
+    @Id
+    @Column(name = "game_id")
+    val gameId: Long? = null,
+
+    @Id
+    @Column(name = "player_id")
+    val playerId: Long? = null,
+
+    @Id
+    @Column(name = "team_id")
+    val teamId: Long? = null,
+
+    @Column(name = "time_on_ice")
+    val timeOnIce: Int = 0,
+
+    @Column(name = "assists")
+    val assists: Int = 0,
+
+    @Column(name = "goals")
+    val goals: Int = 0,
+
+    @Column(name = "penalty_minutes")
+    val penaltyMinutes: Int = 0,
+
+    @Column(name = "shots")
+    val shots: Int = 0,
+
+    @Column(name = "saves")
+    val saves: Int = 0,
+
+    @Column(name = "power_play_saves")
+    val powerPlaySaves: Int = 0,
+
+    @Column(name = "short_handed_saves")
+    val shortHandedSaves: Int = 0,
+
+    @Column(name = "even_saves")
+    val evenSaves: Int = 0,
+
+    @Column(name = "short_handed_shots_against")
+    val shortHandedShotsAgainst: Int = 0,
+
+    @Column(name = "even_shots_against")
+    val evenShotsAgainst: Int = 0,
+
+    @Column(name = "power_play_shots_against")
+    val powerPlayShotsAgainst: Int = 0,
+
+    @Column(name = "decision")
+    val decision: String? = null,
+
+    @Column(name = "save_percentage")
+    val savePercentage: Double = 0.0,
+
+    @Column(name = "power_play_save_percentage")
+    val powerPlaySavePercentage: Double = 0.0,
+
+    @Column(name = "even_strength_save_percentage")
+    val evenStrengthSavePercentage: Double = 0.0,
+)
+
+data class GameGoalieStatsId(
+    @Id
+    @Column(name = "game_id")
+    val gameId: Long? = null,
+
+    @Id
+    @Column(name = "player_id")
+    val playerId: Long? = null,
+
+    @Id
+    @Column(name = "team_id")
+    val teamId: Long? = null,
+) : Serializable
